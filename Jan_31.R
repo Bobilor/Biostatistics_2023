a7eeb34bae16ebb6aba058c56a18afd21dfe46ff
Bobilor <122546923+Bobilor@users.noreply.github.com>
2023-01-31 10:13:21 -0500

    Add files via upload

---
+++ Jan_31.R
@@ -0,0 +1,15 @@
+# Author: Blessing Obilor, Date: 01/31/23, Purpose: Correlation analysis
+# install external library
+if(!require(devtools)) install.packages("devtools") devtools::install_github("kassambara/ggpubr")
+
+# Load the library
+library("ggpubr")
+
+# Load sample dataset
+my_data <- mtcars 
+
+#plot the correlation value
+ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+
+# Just print the correlation value without plotting
+res <- cor.test(my_data$wt, my_data$mpg, method = "pearson")
