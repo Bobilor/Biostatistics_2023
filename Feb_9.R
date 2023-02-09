31c1371972990e5e7c9330d564aa9490c7880f53
Bobilor <122546923+Bobilor@users.noreply.github.com>
2023-02-09 10:20:25 -0500

    Add files via upload

---
+++ Feb_9.R
@@ -0,0 +1,20 @@
+#Author: Pawar, Date: 02/09/2023, Purpose: To perform ANOVA analysis
+
+
+#Load the library
+library(dplyr)
+
+
+#Load the dataset
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
+
+df<-read.csv(PATH)
+
+df<-df[,2:4]
+
+#Run ANOVA functiom
+anova_one_way <- aov(time~poison, data = df) 
+
+anova_one_way
+
+summary(anova_one_way)
