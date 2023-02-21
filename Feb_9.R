82519d909c1900ff86b2e530b8176e73ef40bc63
Bobilor <122546923+Bobilor@users.noreply.github.com>
2023-02-21 10:34:09 -0500

    Update Feb_9.R

--- Feb_9.R
+++ Feb_9.R
@@ -19,5 +19,5 @@ anova_one_way <- aov(time~poison, data = df)
 anova_one_way
 
 summary(anova_one_way)
-# F value = 20.67, thus is significant
+# F value = 20.67, This is greater than 0.05, thus is significant
 #P value 3.96e-05, this is is less than 0.05, thus, it is significant
