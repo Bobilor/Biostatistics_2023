74cdefa51d91a0979d28b0eb228289729773ea90
Bobilor <122546923+Bobilor@users.noreply.github.com>
2023-02-21 10:31:40 -0500

    Update Feb_9.R

--- Feb_9.R
+++ Feb_9.R
@@ -1,4 +1,4 @@
-#Author: Pawar, Date: 02/09/2023, Purpose: To perform ANOVA analysis
+#Author: Blessing Obilor, Date: 02/09/2023, Purpose: To perform ANOVA analysis
 
 
 #Load the library
@@ -10,6 +10,7 @@ PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poiso
 
 df<-read.csv(PATH)
 
+# specifically choose columns 2 to 4
 df<-df[,2:4]
 
 #Run ANOVA functiom
@@ -18,3 +19,5 @@ anova_one_way <- aov(time~poison, data = df)
 anova_one_way
 
 summary(anova_one_way)
+# F value = 20.67, thus is significant
+#P value 3.96e-05, this is is less than 0.05, thus, it is significant
